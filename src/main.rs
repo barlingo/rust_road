90aeb38c71c407692b6084ea71925151e3d9a9d2
barlingo <16936512+barlingo@users.noreply.github.com>
2022-07-09 20:48:04 -0400

    created default game state

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use rusty_engine::prelude::*;
+
+struct GameState {
+    high_score: u32,
+    current_score: u32,
+    enemy_labels: Vec<String>,
+    spawm_timer: Timer,
+}
+
+impl Default for GameState {
+    fn default() -> Self {
+        Self {
+            high_score: 0,
+            current_score: 0,
+            enemy_labels: Vec::new(),
+            spawm_timer: Timer::from_seconds(1.0, false),
+        }
+    }
+}
+
 fn main() {
     println!("Hello, world!");
+    let mut game = Game::new();
+    // setup game here
+    game.run(GameState::default());
 }
