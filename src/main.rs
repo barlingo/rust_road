ae49f5cfe53c0184ff1e151b0c1f96ff8b11d32a
barlingo <16936512+barlingo@users.noreply.github.com>
2022-07-09 21:04:57 -0400

    created a colission

--- src/main.rs
+++ src/main.rs
@@ -23,12 +23,21 @@ fn main() {
     let mut game = Game::new();
     // setup game here
     let player = game.add_sprite("player", SpritePreset::RacingCarBlack);
-    player.translation = Vec2::new(200.0, 100.0);
+    player.translation = Vec2::new(-200.0, 100.0);
+    player.collision = true;
+    let car1 = game.add_sprite("car1", SpritePreset::RacingCarRed);
+    car1.translation = Vec2::new(400.0, 100.0);
+    car1.collision = true;
     game.add_logic(game_logic);
     game.run(GameState::default());
 }
 
 fn game_logic(engine: &mut Engine, game_state: &mut GameState) {
+    for event in engine.collision_events.drain(..) {
+        println!("{:#?}", event);
+    }
     // game_state.current_score += 1;
+    let player = engine.sprites.get_mut("player").unwrap();
+    player.translation.x += 100.0 * engine.delta_f32;
     // println!("Current score: {}", game_state.current_score);
 }
